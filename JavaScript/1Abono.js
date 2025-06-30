a4b68c283a7ae8e372f87c5bc6492aeb8b2f44df
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-06-30 16:13:10 -0300

    exercicios

---
+++ JavaScript/1Abono.js
@@ -0,0 +1,15 @@
+// Dado o Fluxograma abaixo, desenvolva o algoritmo abaixo na Linguagem JavaScript: 
+
+const read = require('readline-sync');
+
+let salario;
+let abono;
+
+salario = read.questionInt("\nDigite o valor do salário: ", {limitMessage: 'Digite um numero inteiro'});
+console.log("O valor do salário é: " + salario);
+
+abono = read.questionInt("\nDigite o valor do abono salarial: ", {limitMessage: 'Digite um numero inteiro'});
+console.log("O valor do abono salarial é: " + abono);
+
+console.log("Novo Salário (salário + abono): " + (salario + abono));
+
---
+++ JavaScript/4DiferencaProduto.js
@@ -0,0 +1,29 @@
+// Leia quatro valores (n1, n2, n3, n4). A seguir, calcule e mostre a diferença do produto entre o n1 e n2 pelo produto entre o n3 e o n4
+
+const read = require('readline-sync');
+
+let n1;
+let n2;
+let n3;
+let n4;
+
+n1 = read.questionFloat("Digite n1: ")
+console.log ("N1 é: " + n1);
+
+n2 = read.questionFloat("Digite n2: ")
+console.log ("N2 é: " + n2);
+
+n3 = read.questionFloat("Digite n3: ")
+console.log ("N3 é: " + n3);
+
+n4 = read.questionFloat("Digite n4: ")
+console.log ("N4 é: " + n4);
+
+let produton1n2 =  n1 * n2;
+console.log ("O produto entre n1 e n2 é: "+ produton1n2);
+
+let produton3n4 = n3 * n4 ;
+console.log ("O produto de n3 e n4 é: " + produton3n4);
+
+let diferenca = produton1n2 - produton3n4;
+console.log ("O resultado de (n1 * n2) – (n3 * n4) é: " +diferenca )
